fc7c0d40c8945a75cb09094ce7374ff4d93d9e45
Raffi Fadlika <mrfadlika248@gmail.com>
2025-06-05 15:25:40 +0800

    commited for logs

---
+++ src/components/views/logs/Logs.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LogsScreen = () => {
+  return (
+    <div className="text-center py-20 text-gray-500">
+      Log Aktivitas segera hadir
+    </div>
+  );
+};
+
+export default LogsScreen;
